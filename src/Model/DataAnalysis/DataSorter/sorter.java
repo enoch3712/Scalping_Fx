3bf213db092f875df9463aa943885ccc93848578
enoch3712@gmail.com <enoch>
2016-08-29 13:49:51 +0100

    Adding buy/sell sorter

---
+++ src/Model/DataAnalysis/DataSorter/sorter.java
@@ -0,0 +1,10 @@
+package Model.DataAnalysis.DataSorter;
+
+/**
+ * Created by enoch3712. doesnt matter the date
+ */
+public class sorter
+{
+
+
+}
--- src/View/sample/Main.java
+++ src/View/sample/Main.java
@@ -4,6 +4,7 @@ import View.sample.DynamicView.DynamicView;
 import View.sample.StaticView.StaticView;
 import javafx.application.Application;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import View.sample.*;
 
@@ -14,6 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        primaryStage.getIcons().add(new Image("http://static1.squarespace.com/static/55523bbae4b0cdb7d2cfedd3/t/55cce5a2e4b0ff38cd529c96/1439491491383/Stock+Market.png?format=300w"));
         View = IS_STATIC ? StaticView.generatePane(this.getClass(),primaryStage):DynamicView.generatePane(primaryStage);
     }
 
